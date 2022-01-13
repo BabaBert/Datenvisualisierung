cb4002158459d4e225146113435f0949758fc638
Bertold Brödner <36105102+BabaBert@users.noreply.github.com>
2022-01-13 20:00:26 +0100

    Js (#4)
    * Delete Cargo.toml
    
    * images
    
    * Add files via upload
    
    * cleanup
    
    * Revert "Merge branch 'master' into js"
    
    This reverts commit 0a13ae9819deb160b113402e8527c4809c46c91c, reversing
    changes made to 5266ce90e3bd9113c49a1d350790b9f9ad497b5b.
    
    Co-authored-by: Anex.jpg <55655432+Anexjpg@users.noreply.github.com>

--- app.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var app = express();
-
-// Set the MIME type explicitly
-express.static.mime.define({'application/wasm': ['wasm']});
-
-app.use(express.static('./dist'));
-
-app.listen(3000);
--- src/shaders/fragment/globe.glsl
+++
@@ -1,9 +0,0 @@
-
-// Data coming from the vertex shader
-varying highp vec2 vTextureCoord;
-
-// The texture unit to use for the color lookup
-uniform sampler2D uSampler;
-void main() {
-  gl_FragColor = texture2D(uSampler, vTextureCoord);
-}
