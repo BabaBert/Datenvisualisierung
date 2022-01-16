c40d43ef429f645927cf84c81e5dc33b594e00a9
Anex.jpg <55655432+Anexjpg@users.noreply.github.com>
2022-01-16 18:36:31 +0100

    Add files via upload

--- canvas.js
+++ canvas.js
@@ -10,6 +10,27 @@ grd.addColorStop(1, "blue");
 tempx.fillStyle = grd;
 tempx.fillRect(0,0,100,256);
 
+
+var slider = document.getElementById("slider");
+var output = document.getElementById("year");
+
+output.innerHTML = convertYears(slider.value);
+
+slider.oninput = function(){
+    output.innerHTML = convertYears(this.value);
+}
+
+function convertYears(input){
+    var months = ['Januar', 'Februar', 'März', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'Dezember'];
+
+    var temp = parseInt(input/12);
+    var year = temp+1880;
+
+    var month = months[input%12];
+
+
+    return month + " " + year;
+}
 //Schwarz zu Weiß
 /*
 var temp = document.getElementById("temperaturCanvas");
